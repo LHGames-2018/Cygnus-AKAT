17232e565711fc5a38a1953aafd42cce442fc115
Lordtitan001 <43910151+Lordtitan001@users.noreply.github.com>
2018-10-06 12:02:19 -0400

    Update bot.py

--- bot/bot.py
+++ bot/bot.py
@@ -20,11 +20,8 @@ class Bot:
         """
 
         # Write your bot here. Use functions from aiHelper to instantiate your actions.
-        return create_move_action(Point(-1, 0))
-        
-        
-    
-
+        return create_move_action(Point(0, 1))
+      
     def after_turn(self):
         """
         Gets called after executeTurn
