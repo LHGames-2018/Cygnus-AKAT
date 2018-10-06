797430fc78805b6355b7a48e8fb75167011ee6e3
Adlaino <43910250+Adlaino@users.noreply.github.com>
2018-10-06 14:59:31 -0400

    Update bot.py

--- bot/bot.py
+++ bot/bot.py
@@ -19,11 +19,58 @@ class Bot:
             :param visiblePlayers:  The list of visible players.
         """
 
+        print(self.PlayerInfo.Position)
         # Write your bot here. Use functions from aiHelper to instantiate your actions.
-        return create_collect_action(Point(-1, 0))
-      
+        resources = gameMap.print()
+
+        print("PLAYER POSITION: " + str(self.PlayerInfo.Position.x) + ',' + str(self.PlayerInfo.Position.y))
+        for resource in resources:
+            print("RESOURCE FOUND: " + str(resource.Position.x) + "," + str(resource.Position.y))
+
+        print("HOUSE FOUND: " + str(self.PlayerInfo.HouseLocation.x) + "," + str(self.PlayerInfo.HouseLocation.y))
+
+        while self.PlayerInfo.Position.x != resources[0].Position.x and self.PlayerInfo.Position.y != resources[0].Position.y-1:
+
+
+            if self.PlayerInfo.Position.x < resources[0].Position.x:
+                return create_move_action(Point(1, 0))
+
+            if self.PlayerInfo.Position.x > resources[0].Position.x:
+                return create_move_action(Point(-1, 0))
+
+            if self.PlayerInfo.Position.y < resources[0].Position.y-1:
+                return create_move_action(Point(0, -1))
+
+            if self.PlayerInfo.Position.y > resources[0].Position.y-1:
+                return create_move_action(Point(0, 1))
+            
+            
+             return create_collect_action(0,1)
+
+            while self.PlayerInfo.Position.x != resources[1].Position.x and self.PlayerInfo.Position.y != resources[
+                0].Position.y - 1:
+
+                if self.PlayerInfo.Position.x < resources[1].Position.x:
+                    return create_move_action(Point(1, 0))
+
+                if self.PlayerInfo.Position.x > resources[1].Position.x:
+                    return create_move_action(Point(-1, 0))
+
+                if self.PlayerInfo.Position.y < resources[1].Position.y - 1:
+                    return create_move_action(Point(0, -1))
+
+                if self.PlayerInfo.Position.y > resources[1].Position.y - 1:
+                    return create_move_action(Point(0, 1))
+
+            if self.PlayerInfo.Position.y > resources[1].Position.y-1:
+                return create_move_action(Point(0, 1))
+
+                return create_collect_action(0, 1)
+
+
     def after_turn(self):
         """
         Gets called after executeTurn
         """
         pass
+
