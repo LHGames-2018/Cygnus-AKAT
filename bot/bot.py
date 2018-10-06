33e64e6379da374c18fc39fad6fe805f881f33dc
Lordtitan001 <43910151+Lordtitan001@users.noreply.github.com>
2018-10-06 19:42:06 -0400

    Update bot.py

--- bot/bot.py
+++ bot/bot.py
@@ -20,63 +20,7 @@ class Bot:
         """
 
         # Write your bot here. Use functions from aiHelper to instantiate your actions.
-        resources = gameMap.print()
-        print("PLAYER POSITION: " + str(self.PlayerInfo.Position.x) + ',' + str(self.PlayerInfo.Position.y))
-        for resource in resources:
-            print("RESOURCE FOUND: " + str(resource.Position.x) + "," + str(resource.Position.y))
-
-            print("HOUSE FOUND: " + str(self.PlayerInfo.HouseLocation.x) + "," + str(self.PlayerInfo.HouseLocation.y))
-
-            if self.PlayerInfo.CarriedResources == 1000 and self.PlayerInfo.Position.x < self.PlayerInfo.HouseLocation.Position.x:
-                if gameMap.getTileAt(self.PlayerInfo.Position + Point(1, 0)) == TileContent.Wall:
-                    return create_move_action(Point(1, 0))
-                else:
-                    return create_move_action(Point(1, 0))
-
-            if self.PlayerInfo.CarriedResources == 1000 and self.PlayerInfo.Position.x > self.PlayerInfo.HouseLocation.Position.x:
-                if gameMap.getTileAt(self.PlayerInfo.Position + Point(-1, 0)) == TileContent.Wall:
-                    return create_attack_action(Point(-1, 0))
-                else:
-                    return create_move_action(Point(-1, 0))
-
-            if self.PlayerInfo.CarriedResources == 1000 and self.PlayerInfo.Position.y > self.PlayerInfo.HouseLocation.Position.y:
-                if gameMap.getTileAt(self.PlayerInfo.Position + Point(0, 1)) == TileContent.Wall:
-                    return create_attack_action(Point(0, 1))
-                else:
-                    return create_move_action(Point(0, 1))
-
-            if self.PlayerInfo.CarriedResources == 1000 and self.PlayerInfo.Position.y < self.PlayerInfo.HouseLocation.Position.y:
-                if gameMap.getTileAt(self.PlayerInfo.Position + Point(0, -1)) == TileContent.Wall:
-                    return create_attack_action(Point(0, -1))
-                else:
-                    return create_move_action(Point(0, -1))
-
-            if self.PlayerInfo.Position.x < resources[0].Position.x:
-                if gameMap.getTileAt(self.PlayerInfo.Position + Point(1, 0)) == TileContent.Wall:
-                    return create_attack_action(Point(1, 0))
-                else:
-                    return create_move_action(Point(1, 0))
-            if self.PlayerInfo.Position.x > resources[0].Position.x:
-                if gameMap.getTileAt(self.PlayerInfo.Position + Point(-1, 0)) == TileContent.Wall:
-                    return create_attack_action(Point(-1, 0))
-                else:
-
-                    return create_move_action(Point(-1, 0))
-
-            if self.PlayerInfo.Position.y < resources[0].Position.y - 1:
-                if gameMap.getTileAt(self.PlayerInfo.Position + Point(0, 1)) == TileContent.Wall:
-                    return create_attack_action(Point(0, 1))
-                else:
-                    return create_move_action(Point(0, 1))
-
-            if self.PlayerInfo.Position.y > resources[0].Position.y - 1:
-                if gameMap.getTileAt(self.PlayerInfo.Position + Point(0, -1)) == TileContent.Wall:
-                    return create_attack_action(Point(0, -1))
-                else:
-                    return create_move_action(Point(0, -1))
-
-            if self.PlayerInfo.Position.y == resources[0].Position.y-1 and self.PlayerInfo.Position.x == resources[0].Position.x:
-                    return create_collect_action(Point(0, 1))
+       return create_move_action(Point(0, 1))
 
     def after_turn(self):
             """
